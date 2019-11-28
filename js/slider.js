3e19d6b4a320ffa93fd36d8733b7d27db1deff55
DenisFuhr <pacpac007d@gmail.com>
2019-11-28 08:48:32 +0100

    first commit

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+$(".specialties").slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 8000,
+  arrows: true,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-double-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-double-right' aria-hidden='true'></i></button>"
+});
+
+
